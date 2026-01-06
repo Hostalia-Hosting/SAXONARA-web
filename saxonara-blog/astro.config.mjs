9e266314939746128ff1ee5ac5bd7e33a47cfdea
piedracita <arnaldpedros@gmail.com>
2026-01-06 22:42:45 +0100

    test php common astro

--- saxonara-blog/astro.config.mjs
+++ saxonara-blog/astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://saxonara.com',
+  site: 'https://www.saxonara.com',
 
   // Això és el que diu a Astro: "encara que jo estigui a l'arrel del meu PC, a internet viuré a /life"
   base: '/life',
