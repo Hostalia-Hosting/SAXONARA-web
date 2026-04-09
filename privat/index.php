160cbf453d553d56dac3210c7bdbfffac6dc3998
Arnald <arnaldpedros@gmail.com>
2026-04-09 18:03:40 +0200

    fix: correct file path references and enhance contract generation section in privat/index.php

--- privat/index.php
+++ privat/index.php
@@ -9,40 +9,78 @@
     <meta name="description" content="Gestió de SAXONARA.">
     
     <!-- COMMON-HEAD -->
-    <?php include "/common-php/head.html"; ?>
+    <?php include "../common-php/head.html"; ?>
     <!-- /COMMON-HEAD -->
 </head>
 
 <body>
     <!-- COMMON-BODY -->
-    <?php include "/common-php/body.html"; ?>
+    <?php include "../common-php/body.html"; ?>
     <!-- /COMMON-BODY -->
 
     <!-- COMMON-HEADER -->
-    <?php include "/common-php/navigation/cat.html"; ?>
+    <?php include "../common-php/navigation/cat.html"; ?>
     <!-- /COMMON-HEADER -->    
 
     <!-- HERO -->
-    <div class="banner-slider owl-slider" id="banner-slider">
-
-        <!-- COMMON-BANNER-EVENTS -->
-        <?php include "/common-php/banner/events/cat.html"; ?>
-        <!-- /COMMON-BODY -->
-
+    <div class="page-header">
+        <div class="container text-center">
+            <h1 class="page-title">Gestió interna de SAXONARA</h1>
+            <h5>Passi Passi</h5>
+            <ol class="breadcrumb">
+                <li><span class="banner-highlight">Si ets aquí i no ets jo, no hi hauries de ser ;p</span></li>
+            </ol>
+        </div>
     </div>
     <!-- /HERO -->
 
     <div class="main-wrap">
 
+        <div class="section section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-xs-12">
+                        <div class="text-center">
+                            <h3 class="section-title">Generar Contractes</h3>
+                            <p>Selecciona l'idioma:</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="blog-grid">
+                        <div class="col-xs-6 col-sm-2 col-md-2 blog-grid-item">
+                            <div class="button-post-container text-center">
+                                <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdb9ZyWTzFk_lgozKJmUsyFjF5DdGFVnD8tbW8qSpUlF9d4DA/viewform?usp=dialog">Català</a>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-2 col-md-2 blog-grid-item">
+                            <div class="button-post-container text-center">
+                                <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLScJ85Sl4hAVVjCu_XGgwohXcCDdni4LKoBLFAByZFTr2YGU8g/viewform?usp=dialog">Castellà</a>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-2 col-md-2 blog-grid-item">
+                            <div class="button-post-container text-center">
+                                <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSe--bMga5NGr9JDDbfF5sVtaFGrb4b8WWgDB0AcR3fCiptoIg/viewform?usp=dialog">Anglès</a>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-2 col-md-2 blog-grid-item">
+                            <div class="button-post-container text-center">
+                                <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSfcuRZoFYu6VTY5_ocV-FNRtqf1pV6ifwTIZqC3VByIL_SCZA/viewform?usp=dialog">Francès</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </div>
 
     <!-- FOOTER -->
-    <?php include "/common-php/footer/es.html"; ?>
+    <?php include "../common-php/footer/es.html"; ?>
     <!-- /FOOTER -->
 
     <!-- COMMON-BODY-SCRIPT -->
-    <?php include "/common-php/body-script.html"; ?>
+    <?php include "../common-php/body-script.html"; ?>
     <!-- /COMMON-BODY-SCRIPT -->
     
 </body>
