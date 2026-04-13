b29c50a201193681757f655c421b19edeecca94c
piedracita <arnaldpedros@gmail.com>
2026-04-13 08:19:09 +0200

    fix: update internal management page layout and language links in privat/index.php

--- privat/index.php
+++ privat/index.php
@@ -13,7 +13,7 @@
     <!-- /COMMON-HEAD -->
 </head>
 
-<body>
+<body class="page">
     <!-- COMMON-BODY -->
     <?php include "../common-php/body.html"; ?>
     <!-- /COMMON-BODY -->
@@ -25,11 +25,9 @@
     <!-- HERO -->
     <div class="page-header">
         <div class="container text-center">
-            <h1 class="page-title">Gestió interna de SAXONARA</h1>
-            <h5>Passi Passi</h5>
-            <ol class="breadcrumb">
-                <li><span class="banner-highlight">Si ets aquí i no ets jo, no hi hauries de ser ;p</span></li>
-            </ol>
+            <h1 class="page-title">Gestió interna</h1>
+            <h5>per SAXONARA</h5>
+
         </div>
     </div>
     <!-- /HERO -->
@@ -41,8 +39,8 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 col-xs-12">
                         <div class="text-center">
-                            <h3 class="section-title">Generar Pressupostos</h3>
-                            <p>Selecciona l'idioma:</p>
+                            <h3 class="section-title">Bàsics de la gestió</h3>
+                            <p><b>Pressupostos</b>, <b>Contractes</b> i <b>Broochures</b> per idioma:</p>
                         </div>
                     </div>
                 </div>
@@ -56,21 +54,21 @@
                                     <img loading="lazy" class="img-responsive" src="/images/weddings/thumb-ceremony.jpg" alt="Saxofonista per a casaments a Cerimònia de casament">
                                 </a>
                                 <div class="post-content">
-                                    <h4 class="post-title"><a href="/cat/saxofonista-bodes-cerimonia.php">Cerimònia de Casament</a></h4>
+                                    <h4 class="post-title"><a href="/cat/saxofonista-bodes-cerimonia.php">Pressupostos</a></h4>
                                     <div class="post-excerpt">
-                                        <p>Les millors <b>balades i música solemne</b> per a moments com el <strong>camí cap a l'altar</strong>, els <strong>anells</strong> o altres moments de la cerimònia de casament. Consulta el repertori per inspirar-te.</p>
+                                        <p>Generar Pressupostos de SAXONARA</p>
                                     </div>
                                     <div class="button-post-container text-center">
-                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-cerimonia.php">+ info</a>
+                                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSfQBFN8AiM3a6Tdo0S_h3gWD__mok40NwXHz__m1co2HrnJGA/viewform?usp=dialog">Català</a>
                                     </div>
                                     <div class="button-post-container text-center">
-                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-cerimonia.php">+ info</a>
+                                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLScaSYAayIK3SeXEwWgC253lHCaagNe9cuo7oJxWr0r27zt0iw/viewform?usp=dialog">Castellà</a>
                                     </div>
                                     <div class="button-post-container text-center">
-                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-cerimonia.php">+ info</a>
+                                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSeWjmHl-DcAkHB1os2fJFN2cwVIFULd0qD_x1iVfZXInhVL6w/viewform?usp=dialog">Anglès</a>
                                     </div>
                                     <div class="button-post-container text-center">
-                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-cerimonia.php">+ info</a>
+                                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdUfys6aumncwRrYmxvj-7U1kJAGP7dT8NG71upuHkhJa3unw/viewform?usp=dialog">Francès</a>
                                     </div>
                                 </div>
                             </article>
@@ -78,16 +76,25 @@
 
                         <div class="col-xs-12 col-sm-6 col-md-4 blog-grid-item">
                             <article class="post">
-                                <a class="post-thumb" href="/cat/saxofonista-bodes-aperitiu.php">
+                                <a class="post-thumb" href="#">
                                     <img loading="lazy" class="img-responsive" src="/images/weddings/thumb-cocktail.jpg" alt="Música en Aperitiu de casament amb saxofonista">
                                 </a>
                                 <div class="post-content">
-                                    <h4 class="post-title"><a href="/cat/saxofonista-bodes-aperitiu.php">Aperitiu o Còctel</a></h4>
+                                    <h4 class="post-title"><a href="#">Contractes</a></h4>
                                     <div class="post-excerpt">
-                                        <p>T'agradaria un aperitiu estil <strong>Jazz</strong>, a veu i saxofon, o el prefereixes estil <strong>House Eivissa</strong>? Adapto el repertori al vostre gust, sempre <strong>elegant</strong>, professionalitat i de <b>qualitat</b>.</p>
+                                        <p>Generar Contractes de SAXONARA</p>
                                     </div>
                                     <div class="button-post-container text-center">
-                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-aperitiu.php">+ info</a>
+                                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdb9ZyWTzFk_lgozKJmUsyFjF5DdGFVnD8tbW8qSpUlF9d4DA/viewform?usp=dialog">Català</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLScJ85Sl4hAVVjCu_XGgwohXcCDdni4LKoBLFAByZFTr2YGU8g/viewform?usp=dialog">Castellà</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSe--bMga5NGr9JDDbfF5sVtaFGrb4b8WWgDB0AcR3fCiptoIg/viewform?usp=dialog">Anglès</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSfcuRZoFYu6VTY5_ocV-FNRtqf1pV6ifwTIZqC3VByIL_SCZA/viewform?usp=dialog">Francès</a>
                                     </div>
                                 </div>
                             </article>
@@ -95,16 +102,25 @@
 
                         <div class="col-xs-12 col-sm-6 col-md-4 blog-grid-item">
                             <article class="post">
-                                <a class="post-thumb" href="/cat/saxofonista-bodes-festa.php">
-                                    <img loading="lazy" class="img-responsive" src="/images/weddings/thumb-party.jpg" alt="Saxofonista i DJ en festa de casament">
+                                <a class="post-thumb" href="#">
+                                    <img loading="lazy" class="img-responsive" src="/images/weddings/thumb-party.jpg" alt="Música en Aperitiu de casament amb saxofonista">
                                 </a>
                                 <div class="post-content">
-                                    <h4 class="post-title"><a href="/cat/saxofonista-bodes-festa.php">Festa / Barra Lliure</a></h4>
+                                    <h4 class="post-title"><a href="#">Brochures</a></h4>
                                     <div class="post-excerpt">
-                                        <p>Treballo amb el DJ de la vostra elecció, li facilito la meva música i microfon sense fil, per <b>saltar a la pista de ball i animar la festa</b>. No et perdis el meu xou de <strong>saxo amb llums</strong>.</p>
+                                        <p>Enllaços Brochures de SAXONARA</p>
                                     </div>
                                     <div class="button-post-container text-center">
-                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-festa.php">+ info</a>
+                                        <a class="btn btn-border btn-black button-post-center" href="https://drive.google.com/open?id=10uWtTkaMsLv6Hp4C1KjXOTp_8q_IUBHV&usp=drive_fs">Català</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="https://drive.google.com/open?id=10uUhZtuj96SCPp5Ss-Vknjy9UfgRl0VX&usp=drive_fs">Castellà</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="https://drive.google.com/open?id=10yyXXjaG984iQZl0jR-KawI0Jl_0b_Qv&usp=drive_fs">Anglès</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="https://drive.google.com/open?id=1B8ZLBXkt_3pVgAPawc4ADfaMQonpSPXd&usp=drive_fs">Francès</a>
                                     </div>
                                 </div>
                             </article>
@@ -112,49 +128,7 @@
 
                     </div>
                 </div>
-                <div class="row">
-                    <div class="button-post-container text-center">
-                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSfQBFN8AiM3a6Tdo0S_h3gWD__mok40NwXHz__m1co2HrnJGA/viewform?usp=dialog">Català</a>
-                    </div>
-                    <div class="button-post-container text-center">
-                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLScaSYAayIK3SeXEwWgC253lHCaagNe9cuo7oJxWr0r27zt0iw/viewform?usp=dialog">Castellà</a>
-                    </div>
-                    <div class="button-post-container text-center">
-                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSeWjmHl-DcAkHB1os2fJFN2cwVIFULd0qD_x1iVfZXInhVL6w/viewform?usp=dialog">Anglès</a>
-                    </div>
-                    <div class="button-post-container text-center">
-                        <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdUfys6aumncwRrYmxvj-7U1kJAGP7dT8NG71upuHkhJa3unw/viewform?usp=dialog">Francès</a>
-                    </div>
-                </div>
-            </div>
-        </div>
 
-        <div class="section section-padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2 col-xs-12">
-                        <div class="text-center">
-                            <h3 class="section-title">Generar Contractes</h3>
-                            <p>Selecciona l'idioma:</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="blog-grid">
-                        <div class="button-post-container text-center">
-                            <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdb9ZyWTzFk_lgozKJmUsyFjF5DdGFVnD8tbW8qSpUlF9d4DA/viewform?usp=dialog">Català</a>
-                        </div>
-                        <div class="button-post-container text-center">
-                            <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLScJ85Sl4hAVVjCu_XGgwohXcCDdni4LKoBLFAByZFTr2YGU8g/viewform?usp=dialog">Castellà</a>
-                        </div>
-                        <div class="button-post-container text-center">
-                            <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSe--bMga5NGr9JDDbfF5sVtaFGrb4b8WWgDB0AcR3fCiptoIg/viewform?usp=dialog">Anglès</a>
-                        </div>
-                        <div class="button-post-container text-center">
-                            <a class="btn btn-border btn-black button-post-center" href="https://docs.google.com/forms/d/e/1FAIpQLSfcuRZoFYu6VTY5_ocV-FNRtqf1pV6ifwTIZqC3VByIL_SCZA/viewform?usp=dialog">Francès</a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         
