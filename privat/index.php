4eff8c549aa859f17f366b881fc8180e56b002a4
piedracita <arnaldpedros@gmail.com>
2026-04-13 07:59:53 +0200

    no hauria de canviar res

--- privat/index.php
+++ privat/index.php
@@ -46,6 +46,72 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="row">
+                    <div class="blog-grid">
+
+                        <div class="col-xs-12 col-sm-6 col-md-4 blog-grid-item">
+                            <article class="post">
+                                <a class="post-thumb" href="/cat/saxofonista-bodes-cerimonia.php">
+                                    <img loading="lazy" class="img-responsive" src="/images/weddings/thumb-ceremony.jpg" alt="Saxofonista per a casaments a Cerimònia de casament">
+                                </a>
+                                <div class="post-content">
+                                    <h4 class="post-title"><a href="/cat/saxofonista-bodes-cerimonia.php">Cerimònia de Casament</a></h4>
+                                    <div class="post-excerpt">
+                                        <p>Les millors <b>balades i música solemne</b> per a moments com el <strong>camí cap a l'altar</strong>, els <strong>anells</strong> o altres moments de la cerimònia de casament. Consulta el repertori per inspirar-te.</p>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-cerimonia.php">+ info</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-cerimonia.php">+ info</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-cerimonia.php">+ info</a>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-cerimonia.php">+ info</a>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-4 blog-grid-item">
+                            <article class="post">
+                                <a class="post-thumb" href="/cat/saxofonista-bodes-aperitiu.php">
+                                    <img loading="lazy" class="img-responsive" src="/images/weddings/thumb-cocktail.jpg" alt="Música en Aperitiu de casament amb saxofonista">
+                                </a>
+                                <div class="post-content">
+                                    <h4 class="post-title"><a href="/cat/saxofonista-bodes-aperitiu.php">Aperitiu o Còctel</a></h4>
+                                    <div class="post-excerpt">
+                                        <p>T'agradaria un aperitiu estil <strong>Jazz</strong>, a veu i saxofon, o el prefereixes estil <strong>House Eivissa</strong>? Adapto el repertori al vostre gust, sempre <strong>elegant</strong>, professionalitat i de <b>qualitat</b>.</p>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-aperitiu.php">+ info</a>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-4 blog-grid-item">
+                            <article class="post">
+                                <a class="post-thumb" href="/cat/saxofonista-bodes-festa.php">
+                                    <img loading="lazy" class="img-responsive" src="/images/weddings/thumb-party.jpg" alt="Saxofonista i DJ en festa de casament">
+                                </a>
+                                <div class="post-content">
+                                    <h4 class="post-title"><a href="/cat/saxofonista-bodes-festa.php">Festa / Barra Lliure</a></h4>
+                                    <div class="post-excerpt">
+                                        <p>Treballo amb el DJ de la vostra elecció, li facilito la meva música i microfon sense fil, per <b>saltar a la pista de ball i animar la festa</b>. No et perdis el meu xou de <strong>saxo amb llums</strong>.</p>
+                                    </div>
+                                    <div class="button-post-container text-center">
+                                        <a class="btn btn-border btn-black button-post-center" href="/cat/saxofonista-bodes-festa.php">+ info</a>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+
+                    </div>
+                </div>
                 <div class="row">
                     <div class="blog-grid">
                         <div class="col-xs-6 col-sm-2 col-md-2 blog-grid-item">
